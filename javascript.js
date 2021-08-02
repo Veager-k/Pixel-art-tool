5de78600ca55ac85c188ed0b82b86a370398659d
Veager <viliussaukstas@gmail.com>
2021-08-02 17:29:54 +0300

    added clear button

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@
     To add:
     input via hover or click
     clear button
-    color > rainbow mode
+    color
     shading
     scaling pixel count
     appealing visual design
@@ -11,19 +11,11 @@
 
 const grid = document.querySelector(".grid");
 
-
-let rainbowMode = false;
-const rainbowButton = document.querySelector("#rainbowButton");
-rainbowButton.addEventListener("click", () => toggleRainbow())
-function toggleRainbow(){
-    if(rainbowMode) rainbowMode = false;
-    else rainbowMode = true;
-}
-
 for(let i=0;i<16*16;i++){
     const div = document.createElement("div");
     div.style.border = "0.05em solid black";
     div.addEventListener("mouseover", (e) => color_cell(e));
+    div.classList.add("cell");
     div.id = i;
     grid.appendChild(div);
 }
@@ -36,4 +28,24 @@ function color_cell(e){
         e.target.style.backgroundColor = "#"+colorR+colorG+colorB;
     }
     else e.target.style.backgroundColor = "black";
+}
+
+let rainbowMode = false;
+const rainbowButton = document.querySelector("#rainbowButton");
+rainbowButton.addEventListener("click", () => toggleRainbow())
+function toggleRainbow(){
+    if(rainbowMode) rainbowMode = false;
+    else rainbowMode = true;
+}
+
+
+const clearButton = document.querySelector("#clearButton");
+clearButton.addEventListener("click", ()=>clear());
+function clear(){
+    console.log("clear function");
+    const allCells = document.querySelectorAll(".cell");
+    allCells.forEach((cell)=>{
+        console.log("clear function 123");
+        cell.style.backgroundColor = "white";
+    })
 }
