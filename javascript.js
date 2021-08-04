1a8a7bca401d1cda036c53f1998e69e0bb1dfaca
Veager <viliussaukstas@gmail.com>
2021-08-04 16:20:57 +0300

    added shading functionality, had to rewrite to use hsl instead of rgb

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 /*
     To add:
     input via hover or click
-    clear button
     color
     shading
     scaling pixel count
@@ -14,6 +13,10 @@ const grid = document.querySelector(".grid");
 for(let i=0;i<16*16;i++){
     const div = document.createElement("div");
     div.style.border = "0.05em solid black";
+    div.style.backgroundColor = "white";
+    div.dataset.hue = 0;
+    div.dataset.sat = 0;
+    div.dataset.shade = 100;
     div.addEventListener("mouseover", (e) => color_cell(e));
     div.classList.add("cell");
     div.id = i;
@@ -22,30 +25,89 @@ for(let i=0;i<16*16;i++){
 
 function color_cell(e){
     if(rainbowMode){
-        let colorR = (Math.floor(Math.random()*16)).toString(16);
-        let colorG = (Math.floor(Math.random()*16)).toString(16);
-        let colorB = (Math.floor(Math.random()*16)).toString(16);
-        e.target.style.backgroundColor = "#"+colorR+colorG+colorB;
+        let newColor = Math.random()*360;
+        e.target.dataset.hue = newColor;
+        e.target.dataset.sat = 100;
+        e.target.dataset.shade = 50;
+        e.target.style.backgroundColor = `hsl(${newColor}, 100%, 50%)`;
+
     }
-    else e.target.style.backgroundColor = "black";
+    else if(shadeMode){
+        e.target.dataset.shade = parseInt(e.target.dataset.shade)-10;
+        // if a cell is shaded to full white > reset saturation
+        if(e.target.dataset.shade<=0){
+            e.target.dataset.shade = 0;
+            e.target.dataset.sat = 0;
+        }
+        e.target.style.backgroundColor = `hsl(${e.target.dataset.hue}, ${e.target.dataset.sat}%, ${e.target.dataset.shade}%)`;
+    }
+    else if(lightMode){
+        e.target.dataset.shade = parseInt(e.target.dataset.shade)+10;
+        // if a cell is shaded to full white > reset saturation
+        if(e.target.dataset.shade>=100){
+            e.target.dataset.shade = 100;
+            e.target.dataset.sat = 0;
+        }
+        e.target.style.backgroundColor = `hsl(${e.target.dataset.hue}, ${e.target.dataset.sat}%, ${e.target.dataset.shade}%)`;
+    }
+    else{
+        e.target.style.backgroundColor = "black";
+        e.target.dataset.sat = 0;
+        e.target.dataset.shade = 0;
+    }
 }
 
+//rainbow toggle
 let rainbowMode = false;
 const rainbowButton = document.querySelector("#rainbowButton");
 rainbowButton.addEventListener("click", () => toggleRainbow())
 function toggleRainbow(){
     if(rainbowMode) rainbowMode = false;
-    else rainbowMode = true;
+    else{
+        rainbowMode = true;
+        //resets other buttons so modes don't overlap
+        shadeMode = false;
+        lightMode = false;
+    }
 }
 
+// shade toggle
+let shadeMode = false;
+const shadeButton = document.querySelector("#shadeButton");
+shadeButton.addEventListener("click", () => toggleShade());
+function toggleShade(){
+    if(shadeMode) shadeMode = false;
+    else{
+        shadeMode = true;
+        //resets other buttons so modes don't overlap
+        rainbowMode = false;
+        lightMode = false;
+    }
+}
 
+//lighten toggle
+let lightMode = false;
+const lightButton = document.querySelector("#lightButton");
+lightButton.addEventListener("click", () => toggleLight());
+function toggleLight(){
+    if(lightMode) lightMode = false;
+    else{
+        lightMode = true;
+        //resets other buttons so modes don't overlap
+        rainbowMode = false;
+        shadeMode = false
+    }
+}
+
+//clear grid function + button
 const clearButton = document.querySelector("#clearButton");
 clearButton.addEventListener("click", ()=>clear());
 function clear(){
-    console.log("clear function");
     const allCells = document.querySelectorAll(".cell");
     allCells.forEach((cell)=>{
-        console.log("clear function 123");
+        cell.dataset.hue = 0;
+        cell.dataset.sat = 0;
+        cell.dataset.shade = 100;
         cell.style.backgroundColor = "white";
     })
 }
